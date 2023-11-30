ce363599181a817265b657e753778be770586f34
martin.kelko <martin2kelko@gmail.com>
2023-11-30 15:34:08 +0100

    fix: add run configuration for `__init__`

--- app/post.py
+++ app/post.py
@@ -57,8 +57,15 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     #conn.commit()
     post = db.query(models.Post).filter(models.Post.id == id)
 
+    post = post_query.first()
+
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} does not exist')
+
+    if post.owner_id != oauth2.get_current_user.id:
+        raise HTTPException (status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized")
+
+    post.delete (synchronize_session=False)
 
     post.delete(synchronize_session=False)
     db.commit()
